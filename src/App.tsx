69d43edca554f334a438b9a43e0bccebd39e0047
Pinaki Majumder <pinaki3.majumder@gmail.com>
2024-10-30 15:02:42 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { useState } from "react";
+import { Modal } from "./components/modal/Modal";
+
+function App() {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <h1>Hey, click on the button to open the modal.</h1>
+      <button className="openModalBtn" onClick={() => setModalOpen(true)}>
+        Open
+      </button>
+
+      <Modal
+        open={modalOpen}
+        titleContent={<h3> Modal Title </h3>}
+        cancelFn={() => setModalOpen(false)}
+        content={
+          <>
+            <h2>This is a modal</h2>
+            <p>You can close it by pressing Escape key, pressing close, or clicking outside the modal.</p>
+          </>
+
+        }
+      />
+    </div>
+  );
+}
+
+export default App;
